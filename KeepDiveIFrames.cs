e44690e09183fecd89879d0e7d9e426ab6fbc1c9
TheMathGeek_314 <16294237+TheMathGeek314@users.noreply.github.com>
2025-02-27 18:09:15 -0600

    Update Satchel version

--- KeepDiveIFrames.cs
+++ KeepDiveIFrames.cs
@@ -5,7 +5,7 @@ using Satchel                                                                   
 namespace KeepDiveIFrames                                                                                       {
     public class KeepDiveIFrames: Mod                                                                           {
         new public string GetName() => "KeepDiveIFrames"                                                        ;
-        public override string GetVersion() => "1.0.0.0"                                                        ;
+        public override string GetVersion() => "1.0.0.1"                                                        ;
         public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)    {
             On.PlayMakerFSM.OnEnable += editFSM                                                                 ;}
         private void editFSM(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self)                             {
